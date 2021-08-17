a42214cf86d1186f4d222b37feed79dbb354b52e
손예진 <shon-yejin@son-yejin-ui-MacBookPro.local>
2021-08-17 21:01:16 +0900

    yshon..

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {createStore} from 'redux';
 
 export default createStore(function(state,action){
     if(state===undefined){
-        return {temperature:20,stateOfLogin:'after',stateOfMyInfo:'manage'}
+        return {temperature:20,stateOfLogin:'after',stateOfMyInfo:'update'}
     }
     if(action.type==='AI_SUBMIT'){
         return {...state,term:action.term, category:action.category, money:action.money}
